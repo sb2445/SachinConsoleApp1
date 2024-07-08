566611105fb97e158e4c93f4e92be7ad16a1b197
sachi <sachi@HP>
2024-07-08 21:31:26 +0530

    Added new class file for creation

---
+++ SachinConsoleApp1/Enum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SachinConsoleApp1
+{
+    internal class Enum
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
