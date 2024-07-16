096fa626f405618db275bd87e22d83a182f9b994
SachinBansode <sachinbansode2445@gmail.com>
2024-07-16 14:21:14 +0530

    Dynamic Polymorphism Assignment

---
+++ SachinConsoleApp1/DynamicPolymorphism.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SachinConsoleApp1
+{
+    class Animal
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing A Shape");
+        }
+
+        class Circle : Animal
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("Circle");
+            }
+        }
+        class Rectangle : Animal
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("Rectangle");
+            }
+
+
+        }
+        internal class DynamicPolymorphism
+        {
+            public static void Main()
+            {
+                Animal obj= new Animal();
+                Animal obj2= new Circle();
+                Animal obj3 = new Rectangle();
+                obj.Draw();
+                obj2.Draw();
+                obj3.Draw();
+            }
+        }
+    }
+}
