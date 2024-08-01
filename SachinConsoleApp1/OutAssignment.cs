1bfb71ce7f2b601da0bba3de74b5e83c0e7b8d10
SachinBansode <sachinbansode2445@gmail.com>
2024-08-01 15:01:51 +0530

    Out Assignment

---
+++ SachinConsoleApp1/OutAssignment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SachinConsoleApp1
+{
+    internal class OutAssignment
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            string text;
+
+            // Call the method to initialize the out parameters
+            InitializeValue(out number, out text);
+
+            // Display the values of the initialized variables
+            Console.WriteLine("Initialized number: " + number);
+            Console.WriteLine("Initialized text: " + text);
+        }
+
+        static void InitializeValue(out int num, out string txt)
+        {
+            // Assign values to the out parameters
+            num = 100;
+            txt = "Hello, World!";
+        }
+    }
+}
+
