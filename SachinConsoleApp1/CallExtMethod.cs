e062fa9d4fe01191bb764e020c8b263a110079cc
SachinBansode <sachinbansode2445@gmail.com>
2024-08-01 14:39:56 +0530

    Extension Method Calling Class

---
+++ SachinConsoleApp1/CallExtMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SachinConsoleApp1
+{
+    internal class CallExtMethod
+    {
+        static void Main()
+        {
+            BaseExtMethod obj = new BaseExtMethod();
+            obj.Addition();
+            obj.Substraction();
+            obj.Multiplication();
+            obj.Division();
+        }
+    }
+}
