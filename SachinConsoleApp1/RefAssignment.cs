e470701354e16763de564f75955c790d2b1c872e
SachinBansode <sachinbansode2445@gmail.com>
2024-08-01 14:31:54 +0530

    Ref Assignment

---
+++ SachinConsoleApp1/RefAssignment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SachinConsoleApp1
+{
+    internal class RefAssignment
+    {
+        public static void IncrementValue(ref int i)
+        {
+            i++;
+        }
+        public static void Main()
+        {
+            int m = 97;
+
+            Console.WriteLine($"Given Number m={m}");
+            IncrementValue(ref m);
+            Console.WriteLine($"After Increment by 1 m={m}");
+        }
+    }
+}
